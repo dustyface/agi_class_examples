baca36061a5d32d4cfae3bd6f8784f3f972e9906
dustyface <dustyface@163.com>
2024-03-29 14:19:26 +0800

    prepare use pylint
    Signed-off-by: dustyface <dustyface@163.com>

--- src/zhihu/RAG/vectordb/rag_bot.py
+++ src/zhihu/RAG/vectordb/rag_bot.py
@@ -1,11 +1,15 @@
+"""
+Base class for RAG bot, RAG Bot for ERNIE, 
+RAG Bot with CascadeDocParser, RAG Bot with CrossEncoder
+"""
 from typing import Union
+from sentence_transformers import CrossEncoder
 from zhihu.RAG.vectordb.chrome_wrapper import ChromaDBWrapper
 from zhihu.RAG.vectordb.embeddings import get_embeddings
 from zhihu.common.api import Session
 from zhihu.RAG.common import parse_paragraph_from_pdf
 from zhihu.RAG.prompt import build_prompt, prompt_template
 from zhihu.RAG.common import cascade_split_text
-
 
 class RAG_Bot:
     prompt_template = prompt_template
@@ -43,4 +47,26 @@ class RAG_Bot_with_CascadeDocParser(RAG_Bot):
         paras = super()._parse_document(in_file, page_numbers=page_numbers)
         return cascade_split_text(paras)
     
-    
+
+class RAG_Bot_with_CrossEncoder(RAG_Bot):
+    def resort_search_result(self, user_query, search_doc_list) -> list[tuple]:
+        cross_encoder_model = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2", max_length=512)
+        scores = cross_encoder_model.predict([(user_query, doc) for doc in search_doc_list])
+        return sorted(
+            zip(scores, search_doc_list),
+            key=lambda x: x[0],
+            reverse=True
+        )
+
+
+    def chat(self, user_query:str, *, top_n=5):
+        result = self._search(user_query, top_n=top_n)
+        # 把搜索到的结果, 使用CrossEncoder重新排序
+        sorted_search_list = self.resort_search_result(user_query, result["documents"][0])
+        prompt = build_prompt(
+            self.prompt_template,
+            info=list(map(lambda x: x[1], sorted_search_list)), 
+            query=user_query
+        )
+        print("user_query=", user_query, "\nprompt=", prompt)
+        return self.session.get_completion(prompt)
